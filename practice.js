929941fb6544a7a6ce672e0ca04066f88ed5f42a
jewelCode <jewelbarua101@gmail.com>
2022-01-28 16:21:50 +0600

    letter grade program

---
+++ practice.js
@@ -0,0 +1,31 @@
+var marks = prompt("Enter The Marks for Know Your Grade");
+
+if(marks>=80)
+{
+    alert("A+");
+}
+else if(marks>=70)
+{
+    alert("A");
+}
+else if(marks>=60)
+{
+    alert("A-");
+}
+else if(marks>=50)
+{
+    alert("B");
+}
+else if(marks>=40)
+{
+    alert("C");
+}
+else if(marks>=33)
+{
+    alert("D");
+}
+
+else{
+    alert("Fails");
+}
+
